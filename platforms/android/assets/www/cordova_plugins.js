ab3ac397337e522a7a7e5045b5a144c1c7c964ea
Andy Franklin <B00262505@studentmail.uws.ac.uk>
2016-11-21 16:26:03 +0000

    Build please

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -1,7 +1,18 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "id": "cordova-sqlite-storage.SQLitePlugin",
+        "file": "plugins/cordova-sqlite-storage/www/SQLitePlugin.js",
+        "pluginId": "cordova-sqlite-storage",
+        "clobbers": [
+            "SQLitePlugin"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
-{};
+{
+    "cordova-sqlite-storage": "1.4.9"
+};
 // BOTTOM OF METADATA
 });
