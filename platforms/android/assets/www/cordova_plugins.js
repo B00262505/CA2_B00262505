378590780504f22a698fe25fa9875028716cedcc
Andy Franklin <B00262505@studentmail.uws.ac.uk>
2016-11-21 16:21:00 +0000

    remove plugin

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -1,18 +1,7 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-    {
-        "id": "cordova-sqlite-storage.SQLitePlugin",
-        "file": "plugins/cordova-sqlite-storage/www/SQLitePlugin.js",
-        "pluginId": "cordova-sqlite-storage",
-        "clobbers": [
-            "SQLitePlugin"
-        ]
-    }
-];
+module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
-{
-    "cordova-sqlite-storage": "1.4.8"
-};
+{};
 // BOTTOM OF METADATA
 });
